882a5aba2e5701ca0884fc63a6f902e13dc9a984
Tuvek <tuvek86@gmail.com>
2019-06-05 20:02:10 +0930

    Clean up

--- MysteryGenerator/Program.cs
+++ MysteryGenerator/Program.cs
@@ -16,7 +16,7 @@ namespace MysteryGenerator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMysteryGen1());
         }
     }
 }
